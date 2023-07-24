39711a895e1ba209f8d7df0745d1feb9734524cb
Big Papa <brett.cole@Rachels-MacBook-Air.local>
2023-07-24 10:01:07 -0400

    added key prop to categoryTitle div

--- app/components/CategoryTitle.tsx
+++ app/components/CategoryTitle.tsx
@@ -15,7 +15,10 @@ const CategoryTitle: FC<ICategoryProps> = ({ categoryRows }) => {
   return (
     <div className='grid grid-cols-6 gap-1 h-28'>
       {categoryRows.map(({ title, id }:ICategoryRow) => (
-        <div className='flex items-center justify-center border border-jeopardyYellow'>
+        <div
+          key={`title-container-${id}`}
+          className='flex items-center justify-center border border-jeopardyYellow'
+        >
           <h1 
             key={`row-${id}`} 
             className="michroma.className text-2xl h-full text-center my-auto"
