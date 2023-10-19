7ddff44822b2ab8d2e5a28c7919660f74030d031
brett.cole <acodercalledcole@gmail.com>
2023-10-19 11:24:50 -0400

    deleted files from moving files and folders around in project

--- app/bible-jeopardy/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="h-screen flex justify-center content-center items-center">
-      <span className="loading loading-ring animate-spin h-20 w-20 text-jeopardyYellow"></span>
-    </div>
-  )
-}
